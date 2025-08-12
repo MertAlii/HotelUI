6802bd44fe75f8430a61e67a13752c3573fd0fa2
MertAlii <Aslancocukmz@gmail.com>
2025-08-12 12:55:09 +0300

    Dto

---
+++ Hotel.DtoLayer/Dtos/RoomDto/RoomUpdateDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel.DtoLayer.Dtos.RoomDto
+{
+    public class RoomUpdateDto
+    {
+        [Required(ErrorMessage = "Lütfen Oda Id'sini Giriniz")]
+        public int RoomId { get; set; }
+        [Required(ErrorMessage = "Lütfen Oda Numarasını Giriniz")]
+        public string RoomNumber { get; set; }
+        public string RoomCoverImage { get; set; }
+        [Required(ErrorMessage = "Lütfen Fiyatı Giriniz")]
+        public int Price { get; set; }
+        [Required(ErrorMessage = "Lütfen Oda Başlığını Giriniz")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Lütfen Yatak Sayısını Giriniz")]
+        public string BedCount { get; set; }
+        [Required(ErrorMessage = "Lütfen Banyo Sayısını Giriniz")]
+        public string BathCount { get; set; }
+        [Required(ErrorMessage = "Lütfen Wifi Sayısını Giriniz")]
+        public string WifiCount { get; set; }
+        [Required(ErrorMessage = "Lütfen Açıklamayı Giriniz")]
+        public string Descritpion { get; set; }
+    }
+}
--- Hotel.WebUI/Controllers/TestimonialController.cs
+++ Hotel.WebUI/Controllers/TestimonialController.cs
@@ -1,15 +1,16 @@
 ﻿using Hotel.WebUI.Models.Staff;
+using Hotel.WebUI.Models.Testimonial;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Text;
 
 namespace Hotel.WebUI.Controllers
 {
-    public class StaffController : Controller
+    public class TestimonialController : Controller
     {
         private readonly IHttpClientFactory _httpClientFactory;
 
-        public StaffController(IHttpClientFactory httpClientFactory)
+        public TestimonialController(IHttpClientFactory httpClientFactory)
         {
             _httpClientFactory = httpClientFactory;
         }
@@ -17,22 +18,22 @@ namespace Hotel.WebUI.Controllers
         public async Task<IActionResult> Index()
         {
             var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("https://localhost:7020/api/Staff");
+            var responseMessage = await client.GetAsync("https://localhost:7020/api/Testimonial");
             if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<List<StaffViewModel>>(jsonData);
+                var values = JsonConvert.DeserializeObject<List<TestimonialViewModel>>(jsonData);
                 return View(values);
             }
             return View();
         }
 
-        public async Task<IActionResult> AddStaff(AddStaffViewModel addStaffViewModel)
+        public async Task<IActionResult> AddTestimonial(TestimonialViewModel model)
         {
             var client = _httpClientFactory.CreateClient();
-            var jsonData = JsonConvert.SerializeObject(addStaffViewModel);
+            var jsonData = JsonConvert.SerializeObject(model);
             StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
-            var responseMessage = await client.PostAsync("https://localhost:7020/api/Staff", content);
+            var responseMessage = await client.PostAsync("https://localhost:7020/api/Testimonial", content);
             if (responseMessage.IsSuccessStatusCode)
             {
                 return RedirectToAction("Index");
@@ -40,10 +41,10 @@ namespace Hotel.WebUI.Controllers
             return View();
         }
 
-        public async Task<IActionResult> DeleteStaff(int id)
+        public async Task<IActionResult> DeleteTestimonial(int id)
         {
             var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.DeleteAsync($"https://localhost:7020/api/Staff?id={id}");
+            var responseMessage = await client.DeleteAsync($"https://localhost:7020/api/Testimonial?id={id}");
             if (responseMessage.IsSuccessStatusCode)
             {
                 return RedirectToAction("Index");
@@ -52,42 +53,29 @@ namespace Hotel.WebUI.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> UpdateStaff(int id)
+        public async Task<IActionResult> UpdateTestimonial(int id)
         {
             var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync($"https://localhost:7020/api/Staff/{id}");
+            var responseMessage = await client.GetAsync($"https://localhost:7020/api/Testimonial/{id}");
             if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<UpdateStaffViewModel>(jsonData);
+                var values = JsonConvert.DeserializeObject<UpdateTestimonialViewModel>(jsonData);
                 return View(values);
             }
             return View();
         }
 
         [HttpPost]
-        public async Task<IActionResult> UpdateStaff(UpdateStaffViewModel updateStaffViewModel)
+        public async Task<IActionResult> UpdateTestimonial(UpdateTestimonialViewModel model)
         {
             var client = _httpClientFactory.CreateClient();
-            var jsonData = JsonConvert.SerializeObject(updateStaffViewModel);
+            var jsonData = JsonConvert.SerializeObject(model);
             StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
             var responseMessage = await client.PutAsync("https://localhost:7020/api/Staff", content);
             if (responseMessage.IsSuccessStatusCode)
             {
                 return RedirectToAction("Index");
-            }
-            return View();
-        }
-
-        public async Task<IActionResult> StaffDetails(int id)
-        {
-            var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync($"https://localhost:7020/api/Staff/{id}");
-            if (responseMessage.IsSuccessStatusCode)
-            {
-                var jsonData = await responseMessage.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<UpdateStaffViewModel>(jsonData);
-                return View(values);
             }
             return View();
         }
