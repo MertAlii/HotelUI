5cc75728b2cf7eafe36b615bbca627915e3a5543
MertAlii <Aslancocukmz@gmail.com>
2025-08-25 21:30:29 +0300

    Service Partial

--- Hotel.WebUI/ViewComponents/Default/_ServicePartial.cs
+++ Hotel.WebUI/ViewComponents/Default/_ServicePartial.cs
@@ -1,11 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Hotel.WebUI.Dtos.RoomDto;
+using Hotel.WebUI.Dtos.ServiceDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Hotel.WebUI.ViewComponents.Default;
 
 public class _ServicePartial : ViewComponent
 {
-    public IViewComponentResult Invoke()
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public _ServicePartial(IHttpClientFactory httpClientFactory)
     {
+        _httpClientFactory = httpClientFactory;
+    }
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var client = _httpClientFactory.CreateClient();
+        var responseMessage = await client.GetAsync("https://localhost:7020/api/Service");
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultServiceDto>>(jsonData);
+            return View(values);
+        }
         return View();
     }
 }
